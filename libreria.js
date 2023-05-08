4b64cb9ca7171c4bb427c5a37faaa62265006dba
Ignacio Jimenez <iniakijimenez@hotmail.com>
2023-05-08 15:56:40 -0300

    mod html

--- libreria.js
+++
@@ -1,8 +0,0 @@
-const books = [
-    { id: 1, title: "La sombra del viento", author: "Carlos Ruiz Zafón", price: 250 },
-    { id: 2, title: "Harry Potter y la piedra filosofal", author: "J.K. Rowling", price: 300 },
-    { id: 3, title: "El código Da Vinci", author: "Dan Brown", price: 200 },
-    { id: 4, title: "El alquimista", author: "Paulo Coelho", price: 150 },
-    { id: 5, title: "Cien años de soledad", author: "Gabriel García Márquez", price: 220 },
-    { id: 6, title: "El principito", author: "Antoine de Saint-Exupéry", price: 120 }
-];
